4ede32de09e9b394518ca4add19ef496bb9b9358
navaloli007 <navaloli007@gmail.com>
2024-12-30 14:46:09 +0530

    lazy loading the watchlist component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./Pages/Home/Home";
-import Watchlist from "./Pages/Watchlist/Watchlist";
+// import Watchlist from "./Pages/Watchlist/Watchlist";
 import Navbar from "./Components/Navbar/Navbar";
-import React, { useState } from "react";
+import React, { lazy, Suspense, useState } from "react";
 import { useEffect } from "react";
 
 export const watchListContext = React.createContext();
 
 function App() {
   const [watchList, setWatchList] = useState(getWatchListFromStorage());
+  const Watchlist = lazy(() => import("./Pages/Watchlist/Watchlist"));
+
   const addToWatchList = (movieObj) => {
     setWatchList([...watchList, movieObj]);
   };
@@ -40,7 +42,15 @@ function App() {
           <Navbar />
           <Routes>
             <Route path="/" element={<Home />}></Route>
-            <Route path="/watchlist" element={<Watchlist />}></Route>
+
+            <Route
+              path="/watchlist"
+              element={
+                <Suspense fallback={<div>WatchList Page is Loading...</div>}>
+                  <Watchlist />
+                </Suspense>
+              }
+            ></Route>
           </Routes>
         </BrowserRouter>
       </watchListContext.Provider>
