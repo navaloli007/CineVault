bb331590d3643a6198c18896c3411597f75ce29c
navaloli007 <navaloli007@gmail.com>
2025-04-06 19:30:05 +0530

    add UI shimmer effect ya loading placeholder.

--- src/Components/MovieCard/MovieCard.jsx
+++ src/Components/MovieCard/MovieCard.jsx
@@ -1,41 +1,60 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { watchListContext } from "../../App";
 
 const MovieCard = (props) => {
   const { movieObj } = props;
-
   const watchListContextVal = useContext(watchListContext);
   const { watchList, addToWatchList, removeFromWatchList } =
     watchListContextVal;
 
-  let moviePoster = movieObj.backdrop_path;
-  let movieTitle = movieObj.title;
+  const [isImageLoaded, setIsImageLoaded] = useState(false);
+
+  const moviePoster = movieObj.backdrop_path;
+  const movieTitle = movieObj.title;
   const moviePosterUrl = `${import.meta.env.VITE_ORIGINAL}${moviePoster}`;
-  let isMovieInWatchList = watchList.find((watchListMovie) => {
-    return watchListMovie.id === movieObj.id;
-  });
+
+  const isMovieInWatchList = watchList.find(
+    (watchListMovie) => watchListMovie.id === movieObj.id
+  );
 
   return (
-    <div
-      className="h-[40vh] w-[200px] bg-cover bg-center rounded-xl flex flex-col justify-between items-end hover:scale-110 duration-300 hover:cursor-pointer"
-      style={{ backgroundImage: `url(${moviePosterUrl})` }}
-    >
-      {!isMovieInWatchList ? (
-        <div
-          onClick={() => addToWatchList(movieObj)}
-          className="m-4 flex justify-end items-center bg-gray-900/60 h-8 w-8 rounded"
-        >
-          &#128525;
-        </div>
-      ) : (
-        <div
-          onClick={() => removeFromWatchList(movieObj)}
-          className="m-4 flex justify-end items-center bg-gray-900/60 h-8 w-8 rounded"
-        >
-          &#10060;
-        </div>
+    <div className="relative h-[40vh] w-[200px] rounded-xl overflow-hidden hover:scale-110 duration-300 hover:cursor-pointer">
+      {/* Shimmer Placeholder */}
+      {!isImageLoaded && (
+        <div className="absolute inset-0 bg-gray-300 animate-pulse rounded-xl" />
       )}
-      <div className="text-xl bg-gray-900 bg-opacity-60 text-white text-center w-full">
+
+      {/* Movie Background Image */}
+      <img
+        src={moviePosterUrl}
+        alt={movieTitle}
+        onLoad={() => setIsImageLoaded(true)}
+        className={`h-full w-full object-cover rounded-xl transition-opacity duration-500 ${
+          isImageLoaded ? "opacity-100" : "opacity-0"
+        }`}
+      />
+
+      {/* Watchlist Button */}
+      <div className="absolute top-2 right-2 z-10">
+        {!isMovieInWatchList ? (
+          <div
+            onClick={() => addToWatchList(movieObj)}
+            className="flex justify-center items-center bg-gray-900/60 h-8 w-8 rounded text-white"
+          >
+            &#128525;
+          </div>
+        ) : (
+          <div
+            onClick={() => removeFromWatchList(movieObj)}
+            className="flex justify-center items-center bg-gray-900/60 h-8 w-8 rounded text-white"
+          >
+            &#10060;
+          </div>
+        )}
+      </div>
+
+      {/* Movie Title */}
+      <div className="absolute bottom-0 left-0 right-0 bg-gray-900/60 text-white text-center py-1 text-xl">
         {movieTitle}
       </div>
     </div>
